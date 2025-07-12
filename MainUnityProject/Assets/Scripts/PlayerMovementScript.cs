5b67b0e21217313f5d8e3df9a93dd4881499aeb7
Programmering <teknisk+programmering-GDC2025-GR4@game.unf.dk>
2025-07-12 16:51:39 +0200

    I am TRYING to fix jumping

--- MainUnityProject/Assets/Scripts/PlayerMovementScript.cs
+++ MainUnityProject/Assets/Scripts/PlayerMovementScript.cs
@@ -1,15 +1,23 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class playerScript : MonoBehaviour
+public class PlayerMovementScript : MonoBehaviour
 {
     public InputAction moveAction;
     public InputAction jumpAction;
     public float walkingSpeed;
     public float jumpingHeight;
     public float gravityAcceleration;
-    
+    public float slideBufferTime;
+    public float rateOfSlide;
     Rigidbody rb;
+
+    public bool jumpable = false;
+    public bool wallSlide = false;
+    public bool touchingSurface = false;
+
+    float timeSpentHanging;
 
     float jumpingForce;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,7 +26,7 @@ public class playerScript : MonoBehaviour
         moveAction.Enable();
         jumpAction.Enable();
         rb = GetComponent<Rigidbody>();
-        jumpingForce = 2 * gravityAcceleration * jumpingHeight;
+        jumpingForce = Mathf.Sqrt(2 * gravityAcceleration * jumpingHeight);
     } 
 
     // Update is called once per frame
@@ -26,21 +34,42 @@ public class playerScript : MonoBehaviour
     {
         ProcessWalking();
         ProcessGravity();
-        if(jumpAction.WasPerformedThisFrame())
+        if(jumpAction.WasPerformedThisFrame() && jumpable)
         {
-            print("jump!");
             Jump();
+        }
+    }
+
+    void ProcessSlidingDownWalls()
+    {
+        if (touchingSurface && wallSlide && !jumpable)
+        {
+            timeSpentHanging += Time.deltaTime;
+        }
+        else
+        {
+            timeSpentHanging = 0;
+        }
+
+        if (timeSpentHanging > slideBufferTime)
+        {
+            Vector3 pos = transform.position;
+            transform.position = new Vector3(pos.x, pos.y - rateOfSlide * Time.deltaTime, pos.z);
+
         }
     }
 
     void ProcessGravity()
     {
-        rb.AddForce(0, -gravityAcceleration, 0, ForceMode.Acceleration);
+        Vector3 vel = rb.linearVelocity;
+        float gravity = gravityAcceleration * Time.deltaTime;
+        rb.linearVelocity = new Vector3(vel.x, vel.y - gravity, vel.z);
     }
 
     void Jump()
     {
-        rb.AddForce(0, jumpingForce, 0, ForceMode.VelocityChange);
+        Vector3 vel = rb.linearVelocity;
+        rb.linearVelocity = new Vector3(vel.x, jumpingForce, vel.z);
     }
 
     void ProcessWalking()
@@ -50,6 +79,37 @@ public class playerScript : MonoBehaviour
         Vector3 side = transform.right;
         Vector3 direction = moveInput.x * side + moveInput.y * forward;
         float coefficient = walkingSpeed;
-        rb.linearVelocity = direction * coefficient;
+        Vector3 walk = direction * coefficient;
+        Vector3 vel = rb.linearVelocity;
+        rb.linearVelocity = new Vector3(walk.x, vel.y, walk.z);
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        print(other.transform.tag);
+        if (other.transform.CompareTag("Jumpable"))
+        {
+            jumpable = true;
+        }else if (other.transform.CompareTag("WallSlide"))
+        {
+            wallSlide = true;
+        }else if (other.transform.CompareTag("Fill"))
+        {
+            touchingSurface = true;
+        }
+    }
+    
+    void OnTriggerExit(Collider other)
+    {
+        if (other.transform.CompareTag("Jumpable"))
+        {
+            jumpable = false;
+        }else if (other.transform.CompareTag("WallSlide"))
+        {
+            wallSlide = false;
+        }else if (other.transform.CompareTag("Fill"))
+        {
+            touchingSurface = false;
+        }
     }
 }
